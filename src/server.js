fea127a53a046e90bad00b46f27f0fae628642fa
opilane <opilane@opilane>
2023-09-29 11:17:16 +0300

    Valmis +

--- src/server.js
+++ src/server.js
@@ -3,12 +3,14 @@ import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
 
+const { json } = require('body-parser');
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
 polka() // You can also use Express
 	.use(
 		compression({ threshold: 0 }),
+		json(),
 		sirv('static', { dev }),
 		sapper.middleware()
 	)
