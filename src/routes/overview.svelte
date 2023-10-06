7e56ea50649b9923cfeefa2dc17b73438fa3572b
ednever <edgarneverovskij@gmail.com>
2023-10-06 12:22:09 +0300

    3 stories done #186198775

--- src/routes/overview.svelte
+++ src/routes/overview.svelte
@@ -1,27 +1,66 @@
 <script context="module">
-    export async function preload({ params }, { token }){
-        if (!token){
-            this.redirect(302, "/login");
+    export async function preload({ params }, { token }) {
+        if (!token) {
+            this.redirect(302, `/login`);
         }
     }
 </script>
 <script>
-    import {post} from "utils.js";
+    import {post} from 'utils.js';
     async function getMyData(){
-        return await post(`auth/getMyData`);
+        return await post('auth/getMyData').then(r=>{
+            r.funds = r.accounts.reduce((funds,account) => funds + account.balance,0)
+            return r
+        })
     }
+    async function getTransactions(){
+        return await post('auth/getTransactions')
+    }
+
 </script>
 {#if process.browser}
     {#await getMyData()}
         Loading...
     {:then my}
         <section>
-            <p>{my.name}</p>
+            <p style="font-size: xx-large">{my.name}</p>
         </section>
         <section>
             My funds
-            <p>{my.funds}</p>
+            <p style="font-size: xx-large; color: {my.funds >= 0 ? 'green':'red'}">{my.funds}</p>
         </section>
-        <pre>{JSON.stringify(my, null, 4)}</pre>
+        <section>
+            <ul>
+                {#each my.accounts as account}
+                    <li>{account.number} ({account.name})</li>
+                {/each}
+            </ul>
+        </section>
+        <section>
+            {#await getTransactions()}
+                loading
+            {:then transactions}
+                <table class="table table-striped table-bordered">
+                    <thead>
+                    <tr>
+                        <th>senderName</th>
+                        <th>amount</th>
+                        <th>createdAt</th>
+                        <th>status</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {#each transactions as transaction}
+                        <tr>
+                            <td><b>{transaction.senderName}</b><br>{transaction.explanation}</td>
+                            <td style="color: {transaction.amount>= 0 ? 'green' : 'red'}">{transaction.amount} {transaction.currency}</td>
+                            <td><b>{transaction.createdAt}</b><br>{transaction.explanation}</td>
+                            <td><b>{transaction.status}</b><br>{transaction.statusDetail}</td>
+                        </tr>
+                    {/each}
+                    </tbody>
+                </table>
+            {/await}
+        </section>
     {/await}
 {/if}
